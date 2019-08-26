c7ffc5aed2d79f87d80f7d24b1ab16720e707a72
Andres Quinto Quinto <andrquqn@suramericana.com.co>
2019-08-26 17:03:38 -0500

    Se actualizan metodos para medicamentos

--- src/controller/medicamento/MedicamentoController.js
+++ src/controller/medicamento/MedicamentoController.js
@@ -45,7 +45,7 @@ module.exports = {
 
     guardarMedicamento: async (req, res) => {
 
-        console.log(req.body);
+        //console.log(req.body);
 
         currentUser = req.body.currentUser;
 
@@ -54,10 +54,70 @@ module.exports = {
             return;
         }
 
-        let medicamento = new Medicamento(req.body.codigo, req.body.nombre, req.body.precioCompra, req.body.precioVenta, req.body.existencias, req.body.unidad, req.body.imagen, req.body.proveedor);
-        await database.ref("medicamentos").push(medicamento);
+        //await database.ref("medicamentos/"+codigo).
 
-        res.send("Medicamento guardado con exito");
+        let medicamento = new Medicamento(req.body.codigo, req.body.nombre, req.body.precioCompra, req.body.precioVenta, req.body.existencias, req.body.unidad, req.body.imagen, req.body.proveedor);
+        await database.ref("medicamentos").child(medicamento.codigo).set({
+            codigo: medicamento.codigo,
+            nombre: medicamento.nombre,
+            precioCompra: medicamento.precioCompra,
+            precioVenta: medicamento.precioVenta,
+            existencias: medicamento.existencias,
+            unidad: medicamento.unidad,
+            imagen: medicamento.imagen,
+            proveedor: medicamento.proveedor
+        }).then(value => {
+            res.send("Medicamento guardado exitosamente");
+        }).catch(error => {
+            res.send(500).send("No se pudo crear el medicamento");
+        });
+
+    },
+
+    actualizarMedicamento: async (req, res) => {
+
+        let medicToUpdate = req.body;
+
+        currentUser = req.body.currentUser;
+
+        if (currentUser && currentUser.rol !== "admin"){
+            res.status(401).send("No esta autorizado, debe ser administrador");
+            return;
+        }
+
+        console.log(medicToUpdate);
+
+        await database.ref().child('/medicamentos/' + medicToUpdate.codigo)
+            .update({ nombre: medicToUpdate.nombre,
+                precioCompra: medicToUpdate.precioCompra,
+                precioVenta: medicToUpdate.precioVenta,
+                existencias: medicToUpdate.existencias,
+                unidad: medicToUpdate.unidad,
+                imagen: medicToUpdate.imagen,
+                proveedor: medicToUpdate.proveedor
+            }).then(value => {
+                res.send("Medicamento actualizado exitosamente")
+            }).catch(error => {
+                res.send(500).send("No se pudo actualizar el medicamento");
+            });
+
+    },
+
+    eliminarMedicamento: async (req, res) => {
+
+        currentUser = req.body.currentUser;
+        let codigo = req.params.codigo;
+
+        if (currentUser && currentUser.rol !== "admin"){
+            res.status(401).send("No esta autorizado, debe ser administrador");
+            return;
+        }
+
+        await database.ref("medicamentos/"+codigo).remove(a => {
+            res.send("Medicamento eliminado exitosamente")
+        }).catch(error => {
+            res.send(500).send("No se pudo eliminar el medicamento");
+        });
 
     },
 
--- src/model/proveedor/Proveedor.js
+++ src/model/proveedor/Proveedor.js
@@ -1,5 +1,6 @@
 module.exports = class Proveedor {
-    constructor(nombre, telefono, correo, direccion){
+    constructor(idProveedor, nombre, telefono, correo, direccion){
+        this.idProveedor = idProveedor;
         this.nombre = nombre;
         this.telefono = telefono;
         this.correo = correo;
